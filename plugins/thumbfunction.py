07357f773e319c23941b09f94cbad22b2ff515cc
Mayank Jaat <153971576+Mayankjat005@users.noreply.github.com>
2024-02-29 21:53:34 +0530

    Update thumbfunction.py

--- plugins/thumbfunction.py
+++ plugins/thumbfunction.py
@@ -1,6 +1,5 @@
 from pyrogram import Client, filters
-from helper.database import find, delthumb, addthumb
-
+from helper.database import find, delthumb, addthumb, viewthumb
 @Client.on_message(filters.private & filters.command(['viewthumb']))
 async def viewthumb(client,message):
 		print(message.chat.id)
